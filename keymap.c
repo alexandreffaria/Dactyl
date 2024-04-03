101e6b7c9ca908200f5e489af815f4c027c2db26
Alexandre F <alexandrefonsecaffaria@gmail.com>
2024-04-03 15:05:35 -0300

    win button on left thumb

--- keymap.c
+++ keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                          KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
                                          RAISE,KC_SPC,                        KC_ENT, LOWER,
                                          KC_TAB,KC_HOME,                         KC_END,  KC_DEL,
-                                         KC_BSPC, KC_LALT,                        KC_LGUI, KC_GRV
+                                         KC_LGUI, KC_LALT,                        KC_BSPC, KC_GRV
     ),
 
     [_LOWER] = LAYOUT_5x6(
