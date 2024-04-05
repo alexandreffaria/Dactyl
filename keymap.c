c501daed70385ede2df41e1f7df45e9527311c8c
Alexandre F <alexandrefonsecaffaria@gmail.com>
2024-04-05 00:34:41 -0300

    reverse quotes positions " '

--- keymap.c
+++ keymap.c
@@ -22,8 +22,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LOWER] = LAYOUT_5x6(
         KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_PSLS,KC_PAST,KC_PMNS,KC_DEL,
         _______,_______,_______,_______,_______,KC_LCBR,                        KC_RCBR, KC_P7 , KC_P8 , KC_P9 ,KC_PPLS,KC_PLUS,
-        _______,KC_HOME,KC_PGUP,KC_PGDN,KC_DQUO ,KC_LBRC,                       KC_RBRC , KC_P4 , KC_P5 , KC_P6 ,KC_PENT,KC_PIPE,
-        _______,_______,_______,_______,KC_QUOT,KC_LPRN,                        KC_RPRN, KC_P1 , KC_P2 , KC_P3 ,KC_PENT ,KC_UNDS,
+        _______,KC_HOME,KC_PGUP,KC_PGDN,KC_QUOT ,KC_LBRC,                       KC_RBRC , KC_P4 , KC_P5 , KC_P6 ,KC_PENT,KC_PIPE,
+        _______,_______,_______,_______,KC_DQUO,KC_LPRN,                        KC_RPRN, KC_P1 , KC_P2 , KC_P3 ,KC_PENT ,KC_UNDS,
                                                 _______,KC_PSCR,            KC_P0, KC_PDOT,
                                                 _______,_______,            _______,_______,
                                                 _______,_______,            _______,_______,
